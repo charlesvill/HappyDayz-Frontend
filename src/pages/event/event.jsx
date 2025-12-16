489f6224d134e502e559c5e034ba0d9abd9a0a1e
Charles Villalpado <charlesvill@g.ucla.edu>
2025-12-16 12:01:07 -0800

    Event page development

--- src/pages/event/event.jsx
+++ src/pages/event/event.jsx
@@ -1,18 +1,19 @@
-// page component builder
-//
-//
-// module component builder
-//
-//
-//
+import { useState } from 'react';
+import { ModuleBuilder } from '../../components/module/moduleBuilder/moduleBuilder';
 
-export function Event() {
-  return (
-    <section>
-      <h4>this is the event page</h4>
-    </section>
-  );
+// current build is to have data passed as prop for testing template and builder fns
+//
+//production build will pull data by event id
+
+function handlePageSetActive() { }
+
+export function Event({ data }) {
+  const [active, setActive] = useState(data.pages[0]);
+  const pageTabNames = data.pages.map((page) => page.title).filter(Boolean);
+  const renderedPage = active.modules.map((module) => (
+    <section>{ModuleBuilder(module.data)}</section>
+  ));
+  return <article></article>;
 }
 
-// work on rendering pipeline for the data made in event initializer
-// create dictionary with keys to components
+// module builder
