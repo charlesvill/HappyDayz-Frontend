4637a9d301f1795d2759fa1d62e6d972977011af
Charles Villalpado <charlesvill@g.ucla.edu>
2026-01-08 21:46:40 -0800

    Refactors for event editing capability

--- src/components/event/eventRenderer/eventRenderer.jsx
+++ src/components/event/eventRenderer/eventRenderer.jsx
@@ -1,21 +1,51 @@
 import { useFetchData } from '../../../utils/hooks/useFetchData';
 import { serverHostName } from '../../../utils/apiUtils';
-import { useContext } from 'react';
+import { useContext, useState, useEffect, createContext } from 'react';
 import { Authorization } from '../../../utils/auth/authProvider';
-import { useParams } from 'react-router-dom';
+import { useParams, useSearchParams } from 'react-router-dom';
 import { Event } from '../../../pages/event/event';
 
+export const EventContext = createContext();
+
 export function EventRenderer() {
-  // pull event id from url params
-  // useEffect to pull event data
-  // pass event data and return event component
-
   // event/userid/eventid
-  const { user } = useContext(Authorization);
-  const { eventid } = useParams();
 
+  const { user } = useContext(Authorization);
+  const [stageData, setStageData] = useState({});
+  const [localData, setLocalData] = useState({});
+  const { eventid } = useParams();
+  // url query param of /?edit => truthy
+  const edit = useSearchParams.has('edit');
+
+  // pass the fn and mode in the context
+  // individual modules will pull the mode and data setter fn from context
+  // each module
   const url = serverHostName() + `/event/${user.id}/${eventid}`;
   console.log('url is: ', url);
   const { loading, data } = useFetchData(url, eventid);
-  return !loading ? <Event data={data} /> : <section>Loading...</section>;
+
+  useEffect(() => {
+    const local = localData.getItem('localEvent');
+    if (local) {
+      // check timestamp localStorage and stageData
+      // if stage data has newer time, update
+      // the local data
+    } else {
+      localStorage.setItem('localEvent', data);
+      setLocalData(data);
+    }
+  }, [stageData]);
+
+  const eventContextValue = {
+    setStageData,
+    edit,
+  };
+
+  return (
+    <>
+      <EventContext.Provider value={eventContextValue}>
+        {!loading ? <Event data={data} /> : <section>Loading...</section>}
+      </EventContext.Provider>
+    </>
+  );
 }
